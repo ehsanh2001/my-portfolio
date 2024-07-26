7fa10d10dd04f1eba1d9e3c8e81d151eda84bdc0
Ehsan Hosseini <ehsanh2001@gmail.com>
2024-07-26 09:37:52 -0400

    Change elements class property to className

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -2,13 +2,19 @@ import { Link, useLocation } from "react-router-dom";
 
 export default function Navbar() {
   const location = useLocation();
+  const styles = {
+    Navbar: {
+      backgroundColor: "lightblue",
+      padding: "10px",
+    },
+  };
 
   return (
-    <nav class="navbar navbar-expand-lg navbar-light bg-light">
-      <div class="container-fluid">
-        <h1 class="navbar-brand mb-0">Ehsan Hosseini</h1>
+    <nav className="navbar navbar-expand-lg navbar-light bg-light">
+      <div className="container-fluid">
+        <h1 className="navbar-brand mb-0">Ehsan Hosseini</h1>
         <button
-          class="navbar-toggler"
+          className="navbar-toggler"
           type="button"
           data-bs-toggle="collapse"
           data-bs-target="#navbarNav"
@@ -16,26 +22,26 @@ export default function Navbar() {
           aria-expanded="false"
           aria-label="Toggle navigation"
         >
-          <span class="navbar-toggler-icon"></span>
+          <span className="navbar-toggler-icon"></span>
         </button>
-        <div class="collapse navbar-collapse" id="navbarNav">
-          <ul class="navbar-nav ms-auto">
-            <li class="nav-item">
+        <div className="collapse navbar-collapse" id="navbarNav">
+          <ul className="navbar-nav ms-auto">
+            <li className="nav-item">
               <Link to="/" className="nav-link">
                 About Me
               </Link>
             </li>
-            <li class="nav-item">
+            <li className="nav-item">
               <Link to="/contact" className="nav-link">
                 Contact
               </Link>
             </li>
-            <li class="nav-item">
+            <li className="nav-item">
               <Link to="/portfolio" className="nav-link">
                 Portfolio
               </Link>
             </li>
-            <li class="nav-item">
+            <li className="nav-item">
               <Link to="/resume" className="nav-link">
                 Resume
               </Link>
