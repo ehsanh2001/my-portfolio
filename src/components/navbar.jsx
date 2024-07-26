e706304041ed8d627770f1e65fd64ea3bd255089
Ehsan Hosseini <ehsanh2001@gmail.com>
2024-07-26 09:53:40 -0400

    Set backgroun image and text color

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -4,15 +4,35 @@ export default function Navbar() {
   const location = useLocation();
   const styles = {
     Navbar: {
-      backgroundColor: "lightblue",
-      padding: "10px",
+      backgroundImage:
+        "url('./nav-background.jpg')" /* Sets the background image */,
+      backgroundSize:
+        "cover" /* Adjusts the background image to cover the entire element */,
+      backgroundPosition: "center" /* Centers the background image */,
+      backgroundRepeat:
+        "no-repeat" /* Prevents the background image from repeating */,
+      height: "200px" /* Sets the height to 100% of the viewport height */,
+    },
+    navLink: {
+      color: "white",
+      fontSize: "1.5rem",
+    },
+    myName: {
+      color: "white",
+      fontSize: "2.5rem",
+      paddingLeft: "2rem",
     },
   };
 
   return (
-    <nav className="navbar navbar-expand-lg navbar-light bg-light">
+    <nav
+      style={styles.Navbar}
+      className="navbar navbar-expand-lg navbar-light bg-light"
+    >
       <div className="container-fluid">
-        <h1 className="navbar-brand mb-0">Ehsan Hosseini</h1>
+        <h1 style={styles.myName} className="navbar-brand mb-0">
+          Ehsan Hosseini
+        </h1>
         <button
           className="navbar-toggler"
           type="button"
@@ -27,22 +47,22 @@ export default function Navbar() {
         <div className="collapse navbar-collapse" id="navbarNav">
           <ul className="navbar-nav ms-auto">
             <li className="nav-item">
-              <Link to="/" className="nav-link">
+              <Link style={styles.navLink} to="/" className="nav-link">
                 About Me
               </Link>
             </li>
             <li className="nav-item">
-              <Link to="/contact" className="nav-link">
+              <Link style={styles.navLink} to="/contact" className="nav-link">
                 Contact
               </Link>
             </li>
             <li className="nav-item">
-              <Link to="/portfolio" className="nav-link">
+              <Link style={styles.navLink} to="/portfolio" className="nav-link">
                 Portfolio
               </Link>
             </li>
             <li className="nav-item">
-              <Link to="/resume" className="nav-link">
+              <Link style={styles.navLink} to="/resume" className="nav-link">
                 Resume
               </Link>
             </li>
