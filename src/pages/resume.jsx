4f86dcfdb75e26ac405f11aeff352068864b4212
Ehsan Hosseini <ehsanh2001@gmail.com>
2024-07-27 11:48:24 -0400

    Create resume page

--- src/pages/resume.jsx
+++ src/pages/resume.jsx
@@ -1,13 +1,47 @@
+import "../App.css";
+
 export default function Resume() {
   return (
     <>
       <div className="container pages">
         <h1>Resume</h1>
         <p>
-          <a href="https://drive.google.com/file/d/1c6YvWwYsY8xO4V4X7X2v8rH4Vz5R3jZB/view?usp=sharing">
+          <a href="https://drive.google.com/file/d/1-ANzWjs6rYbERI-33PKf98m1Bl5rQ6Qv/view?usp=sharing">
             Click here to view my resume.
           </a>
         </p>
+        <h2>Skills</h2>
+        <ul className="white-text">
+          <li>
+            <strong>Programming Languages: </strong> JavaScript, Python, Java
+            C/C++, C#
+          </li>
+          <li>
+            <strong>Web Technologies: </strong> RESTful APIs, GraphQL, Node.js
+            Express.js, HTML, CSS
+          </li>
+          <li>
+            <strong>Databases: </strong> MS SQL Server, MySQL, MongoDB, MS
+            Access
+          </li>
+          <li>
+            <strong>Unit Testing: </strong> pytest, Jest
+          </li>
+          <li>
+            <strong>Concurrent Programming: </strong> C#, Java, Python threads
+            and processes, Javascript async programming
+          </li>
+          <li>
+            <strong>Version Control: </strong> Git, GitHub
+          </li>
+          <li>
+            <strong>Design & Architecture: </strong> OO Design Patterns, SOLID
+            principles, TDD
+          </li>
+          <li>
+            <strong>Agile Methodology: </strong> Scrum
+          </li>
+        </ul>
       </div>
     </>
   );
