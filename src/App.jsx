6dfb65c22e071e3508e96b618c6b54c892027abe
Ehsan Hosseini <ehsanh2001@gmail.com>
2024-07-27 11:48:41 -0400

    Add page footer

--- src/App.jsx
+++ src/App.jsx
@@ -13,6 +13,31 @@ function App() {
           <Outlet />
         </div>
       </div>
+      <footer class="bg-dark text-white p-4 text-center">
+        <a href="https://github.com/ehsanh2001" class="text-white">
+          <img
+            src="../public/github.png"
+            alt="github"
+            width="50"
+            height="50"
+            target="_blank"
+          />
+        </a>
+
+        <a
+          className="ms-2"
+          href="https://www.linkedin.com/in/ehsan-hosseini-/"
+          class="text-white"
+        >
+          <img
+            src="../public/linkedin.png"
+            alt="linkedin"
+            width="50"
+            height="50"
+            target="_blank"
+          />
+        </a>
+      </footer>
     </>
   );
 }
